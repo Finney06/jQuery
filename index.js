6dadc11f10128638288d3604f581335c4461cf8f
Finney Osajere <osafinney21@gmail.com>
2023-10-02 12:45:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,35 @@
 // $(document).on("click", function() {
 //     $("h1").css("color", "purple");
 // });
+
+//HIDE & SHOW
+// $(document).on("click", function() {
+//     $("h1").hide();
+//     $("h1").show();
+// });
+
+//Toggle
+// $(document).on("click", function() {
+//     $("h1").toggle();
+// });
+
+//FADEOUT
+// $(document).on("click", function() {
+//     $("h1").fadeOut();
+// });
+
+// Fade Toggle
+// $(document).on("click", function() {
+//     $("h1").fadeToggle();
+// });
+
+
+//Slide Up / Slide Down / Slide Toggle
+// $(document).on("click", function() {
+//     $("h1").slideToggle();
+// });
+
+// ANIMATE
+$(document).on("click", function() {
+    $("h1").animate({opacity: 0.5, margin: "20"});
+}); //you can only use this for numeric changes 
