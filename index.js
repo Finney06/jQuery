823616857f1561d8528b37de7ecae660a9dc60bb
Finney Osajere <osafinney21@gmail.com>
2023-10-02 11:12:12 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// VANILLA JAVASCRIPT
+// for (var i = 0; i<5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "purple";
+//     });
+// }
+
+
+//jQuery For Clicks
+// $("button").click(function() {
+//     $("h1").css("color", "purple");
+// });
+
+//jQuery For KeyPress
+// $(document).keypress(function(event){
+//     console.log(event.key);
+//     $("h1").text(event.key); 
+// });
+
+//jQuery ON (mouseover h1 to purple)
+// $(document).on("mouseover", function() {
+//     $("h1").css("color", "purple");
+// });
+
+//jQuery ON (click h1 to purple)
+// $(document).on("click", function() {
+//     $("h1").css("color", "purple");
+// });
